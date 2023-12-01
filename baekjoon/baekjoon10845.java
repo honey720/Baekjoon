e4364c2516e37c713f12904b0a925d70f705fd8c
honey720 <honey720@naver.com>
2023-12-01 13:40:53 +0900

    231201

---
+++ baekjoon/baekjoon10845.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class baekjoon10845 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int num = 0;
+            String input = br.readLine();
+            st = new StringTokenizer(input);
+
+            String command = st.nextToken();
+            if(st.hasMoreTokens())
+                num = Integer.parseInt(st.nextToken());
+            Queue(queue, command, num);
+        }
+    }
+
+    public static void Queue(LinkedList<Integer> queue, String command, int num) {
+        switch (command) {
+            case "push": {
+                queue.offer(num);
+                break;
+            }
+            case "pop": {
+                if(queue.isEmpty())
+                    System.out.println("-1");
+                else
+                    System.out.println(queue.poll());
+                break;
+            }
+            case "size": {
+                System.out.println(queue.size());
+                break;
+            }
+            case "empty": {
+                if(queue.isEmpty())
+                    System.out.println("1");
+                else
+                    System.out.println("0");
+                break;
+            }
+            case "front": {
+                if(queue.isEmpty())
+                    System.out.println("-1");
+                else
+                    System.out.println(queue.getFirst());
+                break;
+            }
+            case "back": {
+                if(queue.isEmpty())
+                    System.out.println("-1");
+                else
+                    System.out.println(queue.getLast());
+                break;
+            }
+        }
+    }
+}
