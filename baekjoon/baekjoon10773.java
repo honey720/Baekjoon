e147ef89be903188d9247b0b3f2806aa1e6bf538
honey720 <honey720@naver.com>
2023-12-05 23:09:12 +0900

    231205

---
+++ baekjoon/baekjoon10773.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class baekjoon10773 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if(input == 0) {
+                stack.pop();
+            }
+            else {
+                stack.add(input);
+            }
+        }
+
+        int size = stack.size();
+        for(int j = 0; j < size; j++) {
+            answer += stack.pop();
+        }
+        System.out.print(answer);
+    }
+}
