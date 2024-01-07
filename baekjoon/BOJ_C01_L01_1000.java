019fbeb7db33a423299d8a3a58e877198a8634ce
honey720 <honey720@naver.com>
2024-01-07 14:58:27 +0900

    240107

--- baekjoon/BOJ_C01_L01_1000.java
+++ baekjoon/BOJ_C01_L01_1000.java
@@ -1,8 +1,18 @@
+import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 class BOJ_C01_L01_1000 {
-    public static void main(String[] args) {
-        int a, b, c;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int result = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+        bw.write(result + "");
+        br.close();
+        bw.close();
+
+        /*int a, b, c;
         Scanner scanner = new Scanner(System.in);
 
         a = scanner.nextInt();
@@ -11,5 +21,7 @@ class BOJ_C01_L01_1000 {
         System.out.println(c);
 
         scanner.close();
+
+         */
     }
 }
