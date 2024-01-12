d272479e086e43ce8343b88cb99cd498ab86f700
honey720 <honey720@naver.com>
2024-01-12 18:20:06 +0900

    240112

--- baekjoon/BOJ_C03_L24_1931.java
+++ baekjoon/BOJ_C03_L24_1931.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ_C03_L24_1931 {
 
@@ -14,27 +9,20 @@ public class BOJ_C03_L24_1931 {
         StringBuilder sb = new StringBuilder();
 
         int answer = 0;
-        int end = 0;
-
         int N = Integer.parseInt(br.readLine());
         int[][] time = new int[N][2];
         for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            time[i][0] = s;
-            time[i][1] = e;
-            if(e > end) {
-                end = e;
-            }
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(time, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 return o1[1]!=o2[1] ? o1[1]-o2[1] : o1[0]-o2[0];
             }
         });
-
 
         int idx = 0;
         int prev = 0;
