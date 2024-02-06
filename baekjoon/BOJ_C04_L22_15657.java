37221fd1918f364a7abd22f93e2874eb8fdf3095
honey720 <honey720@naver.com>
2024-02-06 20:04:56 +0900

    240206

--- baekjoon/BOJ_C04_L22_15657.java
+++ baekjoon/BOJ_C04_L22_15657.java
@@ -5,15 +5,14 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ_C04_L22_15654 {
+public class BOJ_C04_L22_15657 {
 
     static StringBuilder sb = new StringBuilder();
     static int N, M;
     static int[] arr;
     static int[] answer;
-    static boolean[] visited;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,35 +21,30 @@ public class BOJ_C04_L22_15654 {
         M = Integer.parseInt(st.nextToken());
         arr = new int[N];
         answer = new int[M];
-        visited = new boolean[N];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        find(N, M, 0);
+        answer[0] = arr[0];
+        find(0,  0, 0);
         bw.write(sb + "");
         br.close();
         bw.flush();
         bw.close();
     }
-
-    public static void find(int n, int m,  int count) {
-        if(m == 0) {
+    public static void find(int m, int start, int count) {
+        if(m == M) {
             for(int i = 0; i < M; i++) {
                 sb.append(answer[i]).append(" ");
             }
             sb.append("\n");
         }
         else {
-            for(int i = 0; i < n; i++) {
-                if(!visited[i]) {
-                    visited[i] = true;
-                    answer[count] = arr[i];
-                    find(n, m - 1, count + 1);
-                    visited[i] = false;
-                }
+            for(int i = start; i < N; i++) {
+                answer[count] = arr[i];
+                find(m + 1, i, count + 1);
             }
         }
     }
