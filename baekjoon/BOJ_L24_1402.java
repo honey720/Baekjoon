ab0415a54ca35934fb4c4822eec26e1187108387
honey720 <honey720@naver.com>
2024-03-08 17:44:46 +0900

    240308

---
+++ baekjoon/BOJ_L24_1402.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_L24_1402 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++) {
+            System.out.println("yes");
+        }
+    }
+}
