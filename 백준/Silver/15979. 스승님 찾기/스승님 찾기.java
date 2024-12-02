1f05efa5fb5f5e072df8735134756e94731c2b2e
honey720 <101120544+honey720@users.noreply.github.com>
2024-12-02 15:21:03 +0900

    [Silver II] Title: 스승님 찾기, Time: 104 ms, Memory: 14284 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/15979. 스승님 찾기/스승님 찾기.java
+++ 백준/Silver/15979. 스승님 찾기/스승님 찾기.java
@@ -25,9 +25,8 @@ public class Main {
     }
 
     public static int getGCD(int num1, int num2) {
-        if(num1 % num2 == 0) {
-            return num2;
-        }
-        return getGCD(num2, num1%num2);
+        if(num1 == 0 || num2 == 0) return num1 + num2;
+        if(num1 > num2) return getGCD(num1 % num2 , num2);
+        return getGCD(num2 % num1, num1);
     }
 }
