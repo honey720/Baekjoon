75f72192ec4035a1f51bf38f584b7bc211287357
Dongheon Lee <101120544+honey720@users.noreply.github.com>
2026-04-01 18:45:56 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String input = br.readLine();
+        for(char c : input.toCharArray()) {
+            if(c <= 'Z') {
+                sb.append((char) (c + 32));
+            }
+            else {
+                sb.append((char) (c - 32));
+            }
+        }
+        System.out.println(sb);
+    }
+}
