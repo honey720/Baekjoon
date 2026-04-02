825e14802750ccaab743fb3d5a88d02adffe005a
Dongheon Lee <101120544+honey720@users.noreply.github.com>
2026-04-02 13:43:06 +0900

    [Bronze V] Title: 몇개고?, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+
+        if ((T >= 12 && T <= 16) && A == 0) System.out.print(320);
+        else System.out.print(280);
+    }
+}
